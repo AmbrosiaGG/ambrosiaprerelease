e711e56d14445c044bbf0366d846afb0c7039335
duckydev <duckydev@ambrosia.gg>
2024-04-21 20:03:28 +0630

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const session = require('express-session')
 import chalk from 'chalk';
 const path = require('path')
-const MongoStore = require('connect-mongo');
-const PORT = process.env.PORT || 3341 // Uselessness of packages
+const PORT = process.env.PORT || 3333 // Uselessness of packages
+
+const app = express()
 
 app.use(require('cors')())
 app.set("view engine", require('ejs'))
-app.use(session({
-  secret: process.env.session_sercret, // Leaky Leaky
-  resave: true,
-  saveUninitialized: false,
-  // store: MongoStore.create({ mongoUrl: process.env.mongoose }) // IF MONGODB ONLY!
-}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // app.use("/api", require('./routers/api'))
 app.use("", require('./routers/index')) // For / Route 
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
-
-// Handlers
-require('./mongoose/init')
 
 /*
 app.use((req, res, next) => {
